827c83fb9bc8d4db7ad3565f54298e4dcc025c63
kgary432 <kgary7373@gmail.com>
2025-02-10 13:41:55 -0500

    circles are now iterative!

--- tutorials/tutorial04/main.js
+++ tutorials/tutorial04/main.js
@@ -3,31 +3,33 @@ let canvasHeight = window.innerHeight;
 
 // in p5.js, the function runs on page load:
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
+  createCanvas(canvasWidth, canvasHeight);
 
-    // invoke any drawing functions inside of setup.
-    // functions should all go between "createCanvas()" and "drawGrid()"
-    draw5Circles();
-    // draw5RedSquares();
-    drawGrid(canvasWidth, canvasHeight);
+  // invoke any drawing functions inside of setup.
+  // functions should all go between "createCanvas()" and "drawGrid()"
+  draw5Circles();
+  // draw5RedSquares();
+  drawGrid(canvasWidth, canvasHeight);
 }
 
 // my first function
 function draw5Circles() {
-    noFill();
-    // fill('red');
-    circle(100, 200, 50); // centerX, centerY, radius
-    circle(100, 250, 50);
-    circle(100, 300, 50);
-    circle(100, 350, 50);
-    circle(100, 400, 50);
+  noFill();
+  // fill('red');
+  let x = 100;
+  let y = 200;
+  let d = 50;
+  while (y <= 400) {
+    circle(x, y, d); // centerX, centerY, radius
+    y += 50;
+  }
 }
 
 function draw5RedSquares() {
-    fill("red");
-    square(320, 200, 50); // topLeftX, topLeftY, width
-    square(320, 250, 50);
-    square(320, 300, 50);
-    square(320, 350, 50);
-    square(320, 400, 50);
+  fill("red");
+  square(320, 200, 50); // topLeftX, topLeftY, width
+  square(320, 250, 50);
+  square(320, 300, 50);
+  square(320, 350, 50);
+  square(320, 400, 50);
 }
