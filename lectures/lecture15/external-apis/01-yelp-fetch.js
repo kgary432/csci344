305f6176a60b776d0124726dd7ac09c618b75d01
kgary432 <kgary7373@gmail.com>
2025-03-05 13:43:49 -0500

    classwork: posts a silly little bear!

--- lectures/lecture15/external-apis/01-yelp-fetch.js
+++ lectures/lecture15/external-apis/01-yelp-fetch.js
@@ -1,12 +1,15 @@
 const showRestaurants = async (location, term, limit) => {
-    const rootURL = 'https://www.apitutor.org/yelp/simple/v3/businesses/search';
-    const endpoint = `${rootURL}?location=${location}&term=${term}&limit=${limit}`;
-    const response = await fetch(endpoint);
-    const jsonData = await response.json();
-    console.log(`Matches for ${term}:`, jsonData);
- };
+  const rootURL = "https://www.apitutor.org/yelp/simple/v3/businesses/search";
+  const endpoint = `${rootURL}?location=${location}&term=${term}&limit=${limit}`;
+  const response = await fetch(endpoint);
+  const jsonData = await response.json();
+  console.log(`Matches for ${term}:`, jsonData);
+  for (let i = 0; (i = jsonData.length); i++) {
+    console.log(jsonData[1]);
+  }
+};
 
-// note that the thai restaurants might 
+// note that the thai restaurants might
 // get printed before the pizza restaurants
-showRestaurants('Asheville,+NC', 'pizza', 5);
-showRestaurants('Asheville,+NC', 'thai', 1);
+showRestaurants("Asheville,+NC", "pizza", 5);
+showRestaurants("Asheville,+NC", "thai", 1);
--- lectures/lecture15/photo-app/create-post.js
+++ lectures/lecture15/photo-app/create-post.js
@@ -2,34 +2,35 @@ import { getAccessToken } from "./get-token.js";
 const rootURL = "https://photo-app-secured.herokuapp.com";
 
 export async function createPost() {
-    // get access token (like logging in) so that you can query for "your data":
-    const token = await getAccessToken(rootURL, "sarah", "password");
+  // get access token (like logging in) so that you can query for "your data":
+  const token = await getAccessToken(rootURL, "kate", "password");
 
-    // data to sent to server:
-    const postData = {
-        image_url: "https://picsum.photos/300/300",
-        caption: "Caption for new post",
-        alt_text: "here is some alt text for the photo!",
-    };
+  // data to sent to server:
+  const postData = {
+    image_url:
+      "https://i.pinimg.com/474x/64/8d/7a/648d7ada46d84aaad5f0ba34d0505a9c.jpg",
+    caption: "i'm beary excited for spring break!",
+    alt_text: "art of a low poly bear",
+  };
 
-    // endpoint:
-    const endpoint = `${rootURL}/api/posts/`;
+  // endpoint:
+  const endpoint = `${rootURL}/api/posts/`;
 
-    // send asynchronous request and wait for response headers:
-    const response = await fetch(endpoint, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(postData),
-    });
+  // send asynchronous request and wait for response headers:
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(postData),
+  });
 
-    // now wait for response body (also asynchronous):
-    const data = await response.json();
+  // now wait for response body (also asynchronous):
+  const data = await response.json();
 
-    // now print to the console:
-    console.log(data);
+  // now print to the console:
+  console.log(data);
 }
 
 createPost();
