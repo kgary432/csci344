9353ac69c07ad3be73c26489d1fef510bdf4e38b
kgary432 <kgary7373@gmail.com>
2025-03-07 12:53:33 -0500

    completed task 1

--- quizzes/quiz02a/task01/main.js
+++ quizzes/quiz02a/task01/main.js
@@ -1,1 +1,7 @@
-// your function here
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
+});
