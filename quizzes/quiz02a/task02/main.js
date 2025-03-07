6fedb67cfa1e6c1ef1dcb01e2b9b52e65f69d0e3
kgary432 <kgary7373@gmail.com>
2025-03-07 12:59:53 -0500

    completed task 2

--- quizzes/quiz02a/task02/main.js
+++ quizzes/quiz02a/task02/main.js
@@ -1,1 +1,23 @@
-// your function here
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("button");
+  const trackList = document.getElementById("track-list");
+  let trackVisible = false;
+
+  button.addEventListener("click", () => {
+    if (trackVisible) {
+      trackList.innerHTML = "";
+    } else {
+      trackList.innerHTML = `
+                <section class="track">
+                    <img src="https://i.scdn.co/image/ab67616d0000b273f6e31941d10e4819d290af41" alt="Album Art">
+                    <div>
+                        <h3>When the Sun Hits</h3>
+                        <p>Slowdive</p>
+                        <p>Souvlaki</p>
+                    </div>
+                </section>
+            `;
+    }
+    trackVisible = !trackVisible;
+  });
+});
