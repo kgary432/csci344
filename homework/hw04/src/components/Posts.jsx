a1000065e67bbb853eee8bdd3e01e5dda4ba6308
kgary432 <kgary7373@gmail.com>
2025-04-04 12:58:55 -0400

    leacture 19 initial commit

--- homework/hw04/src/components/Posts.jsx
+++ homework/hw04/src/components/Posts.jsx
@@ -2,17 +2,34 @@ import React, { useState, useEffect } from "react";
 import { getDataFromServer } from "../server-requests";
 
 export default function Posts({ token }) {
-    const [posts, setPosts] = useState([]);
+  // useState is a function that takes the inital value of a variable as an argument
+  // when useState is invoked, it returns an array with two values
+  // first, the state variable itself: post
+  // second, a function whose job is to set the state varaible and then draw the screen after the variable is set
+  const [posts, setPosts] = useState([]);
+  const [counter, setCounter] = useState(0);
 
-    async function getPosts() {
-        const data = await getDataFromServer(token, "/api/posts");
-        console.log(data);
-        setPosts(data);
-    }
+  function addOneToCounter() {
+    setCounter(counter + 1);
+  }
+  async function getPosts() {
+    const data = await getDataFromServer(token, "/api/posts");
+    console.log(data);
+    setPosts(data);
+  }
 
-    useEffect(() => {
-        getPosts();
-    }, []);
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-    return <div>TODO: output all of the posts: {posts.length}</div>;
+  return (
+    <div>
+      TODO: output all of the posts: {posts.length}
+      {posts.map((post) => (
+        <div>{post.caption}</div>
+      ))}
+      <br />
+      <button onClick={addOneToCounter}>{counter}</button>
+    </div>
+  );
 }
