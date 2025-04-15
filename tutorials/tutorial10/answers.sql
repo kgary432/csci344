9ae75a60adda584e6bb72907b7116210c42162a4
kgary432 <kgary7373@gmail.com>
2025-04-14 22:09:28 -0400

    idek vscode just said i had something new to commit

--- tutorials/tutorial10/answers.sql
+++ tutorials/tutorial10/answers.sql
@@ -1,40 +1,33 @@
 -- Exercise 1 (done for you): Selecting all columns
 SELECT * FROM users;
 
-
-
 -- Exercise 2 (done for you): Selecting some columns
-SELECT id, first_name, last_name 
+SELECT id, first_name, last_name;
 FROM users;
-
-
 
 -- Exercise 3: Sorting
-SELECT id, first_name, last_name 
+SELECT id, first_name, last_name ;
 FROM users;
-GROUP BY last_name
-
-
+GROUP BY last_name;
 
 -- Exercise 4: Filtering
-
-
-
+SELECT id, image_url, user_id;
+FROM users;
+WHERE user_id is 26; 
 
 -- Exercise 5: Filtering with logical operators
-
-
-
+SELECT id, image_url, user_id;
+FROM posts;
+WHERE (user_id is 12) or (user_id is 26);
 
 -- Exercise 6: Using functions in a select statement
-
-
-
+SELECT * FROM posts;
+COUNT
 
 -- Exercise 7: Aggregating data
-
-
-
+SELECT * FROM posts;
+GROUP BY user_id;
+COUNT;
 
 -- Exercise 8: Joining: two tables
 
