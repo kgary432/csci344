62c998cdf2cfaeaca0c351d50904522ada9aebf8
kgary432 <kgary7373@gmail.com>
2025-04-16 13:46:17 -0400

    tutorial 10

--- tutorials/tutorial10/answers.sql
+++ tutorials/tutorial10/answers.sql
@@ -6,45 +6,45 @@ SELECT id, first_name, last_name
 FROM users;
 
 -- Exercise 3: Sorting
-SELECT id, first_name, last_name
-FROM users
-GROUP BY last_name;
+SELECT id, first_name, last_name 
+GROUP BY last_name 
+FROM users;
+
 
 -- Exercise 4: Filtering
-SELECT id, image_url, user_id
-FROM users
+SELECT id, image_url, user_id 
+FROM users 
 WHERE user_id = 26; 
 
 -- Exercise 5: Filtering with logical operators
-SELECT id, image_url, user_id
-FROM posts
+SELECT id, image_url, user_id 
+FROM posts 
 WHERE (user_id = 12) or (user_id = 26);
 
 -- Exercise 6: Using functions in a select statement
-SELECT * FROM posts
-COUNT;
+SELECT * FROM posts 
+COUNT(*);
 
 -- Exercise 7: Aggregating data
-SELECT * FROM posts
-GROUP BY user_id
-COUNT;
+SELECT * FROM posts 
+GROUPBY user_id 
+COUNT(*);
 
 -- Exercise 8: Joining: two tables
 SELECT id, image_url, user_id FROM posts
-INNER JOIN users ON posts.user_id
-WHERE (posts.user_id = user_id = 26) or (posts.user_id = user_id = 12);
+JOIN users ON user_id = id
+WHERE (id = 26) or (id = 12);
 
 -- Exercise 9: More joining practice: two tables
-SELECT id, pub_date FROM posts
-INNER JOIN posts ON following
-WHERE posts.user_id = following.following_id
-WHERE posts.user_id = 26;
+SELECT posts.id, posts.pub_date FROM posts
+JOIN following ON posts.user_id = following.following_id
+WHERE following.user_id = 26;
 
 -- Exercise 10: More joining practice: three tables (Optional)
 
 -- Exercise 11: Inserting records
-INSERT INTO bookmarks(user_id, post_id)
-VALUES (26, 219), (26, 220), (26, 221);
+INSERT INTO bookmarks(user_id, post_id, timestamp)
+VALUES (26, 219, now()), (26, 220, now()), (26, 221, now());
 
 -- Exercise 12: Deleting records
 DELETE FROM bookmarks
@@ -52,8 +52,8 @@ WHERE user_id = 26;
 
 -- Exercise 13: Updating records
 UPDATE users
-SET email = 'knick2022@gmail.com'
-WHERE user_id = 26;
+SET users.email = 'knick2022@gmail.com'
+WHERE users.user_id = 26;
 
 
 
