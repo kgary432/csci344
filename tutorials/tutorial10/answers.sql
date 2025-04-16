94b302af75b7cf0dc22c379b481874e960080065
kgary432 <kgary7373@gmail.com>
2025-04-16 12:55:57 -0400

    work on tutorial 10, may or may not work

--- tutorials/tutorial10/answers.sql
+++ tutorials/tutorial10/answers.sql
@@ -13,12 +13,12 @@ GROUP BY last_name;
 -- Exercise 4: Filtering
 SELECT id, image_url, user_id;
 FROM users;
-WHERE user_id is 26; 
+WHERE user_id = 26; 
 
 -- Exercise 5: Filtering with logical operators
 SELECT id, image_url, user_id;
 FROM posts;
-WHERE (user_id is 12) or (user_id is 26);
+WHERE (user_id = 12) or (user_id = 26);
 
 -- Exercise 6: Using functions in a select statement
 SELECT * FROM posts;
@@ -30,24 +30,21 @@ GROUP BY user_id;
 COUNT;
 
 -- Exercise 8: Joining: two tables
-
-
-
+SELECT id, image_url, user_id FROM posts;
+INNER JOIN users ON posts.user_id;
+WHERE (posts.user_id = user_id = 26) or (posts.user_id = user_id = 12);
 
 -- Exercise 9: More joining practice: two tables
-
-
-
+SELECT id, pub_date FROM posts;
+INNER JOIN posts ON following;
+WHERE posts.user_id = following.following_id;
+WHERE posts.user_id = 26;
 
 -- Exercise 10: More joining practice: three tables (Optional)
 
-
-
-
 -- Exercise 11: Inserting records
-
-
-
+INSERT INTO bookmarks(user_id, post_id);
+VALUES (26, 219), (26, 220), (26, 221);
 
 -- Exercise 12: Deleting records
 
